4244fe80e50fd1f92f850ae3a335750505060acd
Brian Alegre <brialegre@yahoo.com>
2022-04-21 23:18:09 -0700

    Updated Site Preview Picture, removed console.logs, Updated timeSlot texts, fixed saving issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,11 @@
-// variables
-var btnEl = document.querySelectorAll(".btn")
-// Check each button
-// console.log(btnEl)
-
-// Array of times
-var timeSlots = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
-// Check timeSlots
-// console.log(timeSlots)
+// Variables
+var timeSlots = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 
 // Function for making each time block
-function makeTimeblocks(hour, existingTodo = ""){
+function makeTimeblocks(hour, existingTodo = "") {
     // Get the current time
     var currentHour = new Date().getHours();
-
+    
     // Check if current time vs time block
     var presentPastOrFuture = "future";
     if (currentHour > hour) presentPastOrFuture = "past";
@@ -28,35 +21,36 @@ function makeTimeblocks(hour, existingTodo = ""){
     $(`
     <div class="row time-block">
         <div class="hour col-1">${hourName}</div>
-        <textarea name="" id="${hourName}" cols="30" rows="3" class="description col-9 ${presentPastOrFuture}">${existingTodo || ""}</textarea>
+        <textarea name="" id="${hourName}" cols="30" rows="3" class="description col-9 ${presentPastOrFuture}">${
+    existingTodo || ""
+    }</textarea>
         <button class="btn saveBtn col-2">Save</button>
     </div>`)
     );
+
+
 }
 
-
 // Create time block for each hour
-for(var i = 0; i < 9 ; i++){
+for (var i = 0; i < 9; i++) {
     makeTimeblocks(i);
 }
 
 
+// Variables
+var btnEl = document.querySelectorAll(".saveBtn");
+
 // Cycle thru save buttons
-for (var i = 0; btnEl.length; i++) {
-    btnEl[i].addEventListener("click", functionForSaving)
-}     
-        // Verify each click works
-        // alert("clicked me")
+for (var i = 0; i < btnEl.length; i++) {
+    btnEl[i].addEventListener("click", functionForSaving);
+}
 
 
-function functionForSaving (event) {
+// Function For Saving Key Value Pairs
+function functionForSaving(event) {
     // Checking able to get text area content
-    // console.log("target", event.target.parentNode.children[1].value)
-    var todoValue = event.target.parentNode.children[1].value
-    var todoKey = event.target.parentNode.children[1].id
-
-    // check values
-    console.log("key values" + todoKey, todoValue)
+    var todoValue = event.target.parentNode.children[1].value;
+    var todoKey = event.target.parentNode.children[1].id;
 
     // Store to localStorage
     localStorage.setItem(todoKey, todoValue);
